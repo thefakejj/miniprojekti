5c5429a740cc88efe019cdaeaac1d23a79261ce3
viljavai <vilja.vaisanen@helsinki.fi>
2023-11-15 13:57:26 +0200

    fix

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-
+from os import getenv
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("://", "ql://", 1)
 db = SQLAlchemy(app)
--- src/routes.py
+++ src/routes.py
@@ -4,18 +4,12 @@ from sqlalchemy.sql import text
 import methods
 
 
-@app.route("/index")
-def index():
-    return render_template("index.html")
 
-<<<<<<< HEAD
 @app.route("/postbook")
 def post_book():
     return render_template("post.html")
         
-=======
+
 @app.route("/index", methods=["GET"])
 def view():
     return render_template("index.html")
-    
->>>>>>> 38d531cdc335ee41e56c0e7d181ad01ae16cf634
