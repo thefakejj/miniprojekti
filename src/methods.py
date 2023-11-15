3a030893f8c05098c6e5f8121fcff191e17f5038
viljavai <vilja.vaisanen@helsinki.fi>
2023-11-15 13:21:25 +0200

    book post method

--- src/methods.py
+++ src/methods.py
@@ -1,4 +1,8 @@
 from flask import make_response
 
 def post_book(username, key, author, title, year, publisher):
-    sql = "INSERT INTO book "
+    sql = "INSERT INTO book (username, key, author, title, year, publisher) VALUES (:username, :key, :author, :title, :year, :publisher)"
+    db.session.execute(sql, {"username":username, "key":key, "author":author, "title":title, "year":year, "publisher"})
+    db.session.commit()
+    return True
+
