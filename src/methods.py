4017b7e7764cc7b29a146b488a48d7ce12a60031
F4U57O <fabio.sciortino@helsinki.fi>
2023-11-15 13:37:00 +0200

    update methods

--- src/methods.py
+++ src/methods.py
@@ -2,7 +2,13 @@ from flask import make_response
 
 def post_book(username, key, author, title, year, publisher):
     sql = "INSERT INTO book (username, key, author, title, year, publisher) VALUES (:username, :key, :author, :title, :year, :publisher)"
-    db.session.execute(sql, {"username":username, "key":key, "author":author, "title":title, "year":year, "publisher"})
+    db.session.execute(sql, {"username":username, "key":key, "author":author, "title":title, "year":year, "publisher":publisher})
+    db.session.commit()
+    return True
+
+def get_book(username, key, author, title, year, publisher):
+    sql = "SELECT username, key, author, title, year, publisher FROM book"
+    db.session.execute(sql, {"username":username, "key":key, "author":author, "title":title, "year":year, "publisher":publisher})
     db.session.commit()
     return True
 
