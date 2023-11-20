dee635608ee945b5864d9ee4d65f91c4b6658e4a
thefakejj <jasper.laurila@gmail.com>
2023-11-20 18:08:56 +0200

    poistetaan pingaukset

--- run_robot_tests.sh
+++ run_robot_tests.sh
@@ -4,9 +4,9 @@
 poetry run flask run &
 
 # odetetaan, että palvelin on valmiina ottamaan vastaan pyyntöjä
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5000/ping)" != "200" ]];
-  do sleep 1;
-done
+# while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5000/ping)" != "200" ]];
+#   do sleep 1;
+# done
 
 # suoritetaan testit
 poetry run robot src/tests
