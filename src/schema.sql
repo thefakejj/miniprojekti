594722453260764ab969faafae21522636232f0b
viljavai <vilja.vaisanen@helsinki.fi>
2023-11-22 13:23:10 +0200

    references table added

--- src/schema.sql
+++ src/schema.sql
@@ -1,1 +1,18 @@
-CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, key TEXT NOT NULL, author TEXT NOT NULL, title TEXT NOT NULL, year INTEGER NOT NULL, publisher TEXT NOT NULL);
+CREATE TABLE IF NOT EXISTS reference 
+(id INTEGER PRIMARY KEY AUTOINCREMENT, 
+reftype TEXT NOT NULL
+username TEXT, 
+key TEXT NOT NULL, 
+author TEXT NOT NULL, 
+title TEXT NOT NULL, 
+year INTEGER NOT NULL, 
+publisher TEXT NOT NULL,
+volume TEXT 
+series TEXT
+address TEXT
+edition TEXT
+month TEXT
+note TEXT
+school TEXT NOT NULL,
+type TEXT
+);
