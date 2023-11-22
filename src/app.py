6720d7d8016729e245e51f2b912aa8d71325a734
viljavai <vilja.vaisanen@helsinki.fi>
2023-11-22 12:59:31 +0200

    read schema

--- src/app.py
+++ src/app.py
@@ -1,10 +1,26 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
+import sqlite3
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+print(getenv("DATABASE_URL"))
 db = SQLAlchemy(app)
+
+def read_schema(path):
+    with open(path, 'r') as file:
+        commands = file.read().split(';')
+        connection = sqlite3.connect('src/instance/site.db')
+        cursor = connection.cursor()
+        for command in commands:
+            print(command)
+            cursor.execute(command)
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+read_schema('src/schema.sql')
 
 if __name__ == "__main__":
     app.run(debug=True)
