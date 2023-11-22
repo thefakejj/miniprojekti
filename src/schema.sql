8a9cb5bbdafb628b70040499368a21eb858584be
viljavai <vilja.vaisanen@helsinki.fi>
2023-11-22 15:52:45 +0200

    typo

--- src/schema.sql
+++ src/schema.sql
@@ -7,12 +7,12 @@ author TEXT NOT NULL,
 title TEXT NOT NULL, 
 year INTEGER NOT NULL, 
 publisher TEXT NOT NULL,
-volume TEXT 
-series TEXT
-address TEXT
-edition TEXT
-month TEXT
-note TEXT
+volume TEXT,
+series TEXT,
+address TEXT,
+edition TEXT,
+month TEXT,
+note TEXT,
 school TEXT NOT NULL,
 type TEXT
 );
