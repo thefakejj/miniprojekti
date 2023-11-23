8fd1bc74b2b997a5b8412c21dfd805ac04710e79
thefakejj <jasper.laurila@gmail.com>
2023-11-23 11:52:46 +0200

    unit tests have cursor and funny imports

--- src/tests/methods_test.py
+++ src/tests/methods_test.py
@@ -1,13 +1,18 @@
 import unittest
 import methods
-from db_connection import get_database_connection
+from config import DATABASE_FILE_PATH
+import sqlite3
+#from db_connection import get_database_connection
 
 class MethodsTest(unittest.TestCase):
     def setUp(self):
-        self.connection = get_database_connection()
+        #self.connection = get_database_connection()
+        self.connection = sqlite3.connect(DATABASE_FILE_PATH)
+        self.cursor = self.connection.cursor()
+        self.cursor.execute("DROP TABLE reference")
         # self.engine = create_engine('sqlite:///:memory:')
         # self.connection = self.engine.connect()
-        self.connection.execute("CREATE TABLE reference ("
+        self.cursor.execute("CREATE TABLE reference ("
                                 "id INTEGER PRIMARY KEY AUTOINCREMENT,"
                                 "username TEXT,"
                                 "key TEXT,"
@@ -21,3 +26,10 @@ class MethodsTest(unittest.TestCase):
                                 "month TEXT,"
                                 "note TEXT)"
                                 )
+        self.connection.commit()
+    def test_send_and_get_books(self):
+        methods.send_book("user1", "Author1", "Title1", 2022, "Publisher1")
+        methods.send_book("user2", "Author2", "Title2", 2023, "Publisher2")
+
+        books = methods.get_books()
+        self.assertEqual(len(books), 2)
