5d5ab236a3755cfa9fdb868456f3d8097e348aae
F4U57O <fabio.sciortino@helsinki.fi>
2023-11-23 13:43:35 +0200

    try to fix reftype

--- src/methods.py
+++ src/methods.py
@@ -12,21 +12,19 @@ def send_book(username, author, title, year, publisher):
     return True
 
 def get_books():
-    sql = text("SELECT username, key, author, title, year, publisher FROM reference")
+    sql = text("SELECT username, key, author, title, year, publisher FROM reference WHERE reftype LIKE '%book%'")
     result = db.session.execute(sql)
     books = result.fetchall()
     return books
 
-def send_master(username, key, author, title, school, year, type, address, month, note):
-    reftype = "masterthesis"
-    key = generate_key(author,year)
-    sql = text("INSERT INTO reference (reftype, username, key, author, title, school, year, type, address, month, note) VALUES (:reftype, :username, :key, :author, :title, :school, :year, :address, :month, :note)")
-    db.session.execute(sql, {"reftype":reftype,"username":username, "key":key, "author":author, "title":title, "school":school, "year":year, "type":type, "address":address, "month":month, "note":note})
+def send_master(username, author, title, school, year, type, address, month, note):
+    sql = text("INSERT INTO reference (username, key, author, title, school, year, type, address, month, note) VALUES (:username, :key, :author, :title, :school, :year, :address, :month, :note)")
+    db.session.execute(sql, {"username":username, "key":generate_key(author,year), "author":author, "title":title, "school":school, "year":year, "type":type, "address":address, "month":month, "note":note})
     db.session.commit()
     return True
 
 def get_master():
-    sql = text("SELECT username, key, author, title, year, type, address, month, note, school FROM reference")
+    sql = text("SELECT username, key, author, title, year, type, address, month, note, school FROM reference WHERE reftype LIKE '%master%'")
     result = db.session.execute(sql)
     master = result.fetchall()
     return master
