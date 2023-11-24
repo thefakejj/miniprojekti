4bf012ba2259e67a74958a43eace331a57b3454d
viljavai <vilja.vaisanen@helsinki.fi>
2023-11-24 19:15:30 +0200

    master-ref unittest

--- src/tests/methods_test.py
+++ src/tests/methods_test.py
@@ -18,12 +18,12 @@ class MethodsTest(unittest.TestCase):
             CREATE TABLE IF NOT EXISTS reference (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 reftype TEXT NOT NULL,
-                username TEXT,
-                key TEXT NOT NULL,
-                author TEXT NOT NULL,
-                title TEXT NOT NULL,
-                year INTEGER NOT NULL,
-                publisher TEXT NOT NULL,
+                username TEXT, 
+                key TEXT NOT NULL, 
+                author TEXT, 
+                title TEXT, 
+                year INTEGER, 
+                publisher TEXT,
                 volume TEXT,
                 series TEXT,
                 address TEXT,
@@ -38,16 +38,24 @@ class MethodsTest(unittest.TestCase):
 
     def test_send_and_get_books(self):
         with app.app_context():
-            methods.send_book("user1", "Author1", "Title1", 2022, "Publisher1")
-            methods.send_book("user2", "Author2", "Title2", 2023, "Publisher2")
+            methods.send_book("user1", "Author1", "Title1", 2022, "Publisher1", "", "", "", "", "", "")
+            methods.send_book("user2", "Author2", "Title2", 2023, "Publisher2", "", "", "", "", "", "")
 
             books = methods.get_books()
             self.assertEqual(len(books), 2)
     
+    def test_send_and_get_master(self):
+        with app.app_context():
+            methods.send_master("user4", "Author4", "Title4", "School4", 2022, "", "", "", "")
+            methods.send_master("user5", "Author5", "Title5", "School5", 2023, "", "", "", "")
+
+            master = methods.get_master()
+            self.assertEqual(len(master), 2)
+    
     def test_key_generation(self):
         with app.app_context():
-            methods.send_book("user3", "Author3", "Title3", 1987, "Publisher3")
-            methods.send_book("user4", "Author3", "Title4", 1887, "Publisher4")
+            methods.send_book("user3", "Author3", "Title3", 1987, "Publisher3", "", "", "", "", "", "")
+            methods.send_book("user4", "Author3", "Title4", 1887, "Publisher4", "", "", "", "", "", "")
 
             books = methods.get_books()
             key3 = books[0][1]
