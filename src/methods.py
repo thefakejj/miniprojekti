5380953f6b87486752f5dd24233743f22e2b7ee3
viljavai <vilja.vaisanen@helsinki.fi>
2023-11-29 12:59:47 +0200

    Index Co-authored-by: F4U57O

--- src/methods.py
+++ src/methods.py
@@ -25,6 +25,12 @@ def send_master(username, author, title, school, year, type, address, month, not
     db.session.commit()
     return True
 
+def get_references():
+    sql = text("SELECT * FROM reference")
+    result = db.session.execute(sql)
+    references = result.fetchall()
+    return references
+
 def get_master():
     sql = text("SELECT username, key, author, title, year, type, address, month, note, school FROM reference WHERE reftype LIKE '%master%'")
     result = db.session.execute(sql)
