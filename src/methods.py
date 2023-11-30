dead55fac42a619d75b4f845e3dd1fcc0ab558f0
F4U57O <fabio.sciortino@helsinki.fi>
2023-11-30 15:20:46 +0200

    add editmaster and fix editbook

--- src/methods.py
+++ src/methods.py
@@ -76,75 +76,15 @@ def delete_reference(key):
     return True
 
 def edit_book(username, key, author, title, year, publisher, volume, series, address, edition, month, note):
-    sql = text("UPDATE reference SET username = :username, author = :author, title = :title, year = :year, publisher = :publisher, volume = :volume, series = :series, address = :address, edition = :edition, month = :month, note = :note WHERE key = :key")
-    db.session.execute(sql, {"username": username, "key": key, "author": author, "title": title, "year": year, "publisher": publisher, "volume": volume, "series": series, "address": address, "edition": edition, "month": month, "note":note})
+    reftype = "book"
+    sql = text("UPDATE reference SET reftype = :reftype, username = :username, author = :author, title = :title, year = :year, publisher = :publisher, volume = :volume, series = :series, address = :address, edition = :edition, month = :month, note = :note WHERE key = :key")
+    db.session.execute(sql, {"reftype": reftype, "username": username, "key": key, "author": author, "title": title, "year": year, "publisher": publisher, "volume": volume, "series": series, "address": address, "edition": edition, "month": month, "note": note})
     db.session.commit()
     return True
 
-def edit_master(username, key, author, title, school, year, type, address, month):
-    sql = text("UPDATE reference SET username = :username, author = :author, title = :title, school = :school, year = :year, type = :type address = :address, month = :month, note = :note WHERE key = :key")
-    db.session.execute(sql, {"username": username, "key": key, "author": author, "title": title, "school": school, "year": year, "type": type, "address": address, "month": month})
-    db.session.commit()
-    return True
-
-def get_all_references_dict():
-    refs_dict = {}
-    refs_dict["books"] = get_books()
-    refs_dict["masters"] = get_master()
-
-    return refs_dict
-
-def create_book_bibtex_format(book):
-    book_ref_text = f'''@book{{{book[1]},
-author = {{{book[2]}}},
-title = {{{book[3]}}},
-year = {{{book[4]}}},
-publisher = {{{book[5]}}},
-volume = {{{book[6]}}},
-series = {{{book[7]}}},
-address = {{{book[8]}}},
-edition = {{{book[9]}}},
-month = {{{book[10]}}},
-note = {{{book[11]}}}             
-}}
-    '''
-    return book_ref_text
-
-def create_master_bibtex_format(master):
-    master_ref_text = f'''@masterthesis{{{master[1]},
-author = {{{master[2]}}},
-title = {{{master[3]}}},
-year = {{{master[4]}}},
-type = {{{master[5]}}},
-address = {{{master[6]}}},
-month = {{{master[7]}}},
-note = {{{master[8]}}},
-school = {{{master[9]}}}          
-}}
-    '''
-    return master_ref_text
-
-def create_bibtex_text():
-    bibtex_string = ""
-    refs_dict = get_all_references_dict()
-    for book in refs_dict["books"]:
-        bibtex_string += create_book_bibtex_format(book)+"\n"
-
-    for master in refs_dict["masters"]:
-        bibtex_string += create_master_bibtex_format(master)+"\n"
-    return bibtex_string
-
-def create_bibtex_file():
-
-    if os.path.exists("src/outputs/references.bib"):
-        os.remove("src/outputs/references.bib")
-    bibtex_string = create_bibtex_text()
-    with open("src/outputs/references.bib", "w", encoding='utf-8') as bibtex_file:
-        bibtex_file.write(bibtex_string)
-
-#varovasti sitte tän kanssa
-def nuke_db():
-    sql = text("DROP TABLE reference")
-    db.session.execute(sql)
+def edit_master(username, key, author, title, school, year, type, address, month, note):
+    reftype = "master"
+    sql = text("UPDATE reference SET reftype = :reftype, username = :username, author = :author, title = :title, school = :school, year = :year, type = :type, address = :address, month = :month, note = :note WHERE key = :key")
+    db.session.execute(sql, {"reftype": reftype, "username": username, "key": key, "author": author, "title": title, "school": school, "year": year, "type": type, "address": address, "month": month, "note": note})
     db.session.commit()
     return True
