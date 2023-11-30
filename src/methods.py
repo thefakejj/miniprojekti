f4d84cb32125d8923bd531a65d750261647d26ec
thefakejj <jasper.laurila@gmail.com>
2023-11-30 12:08:31 +0200

    merged stuff in main

--- src/methods.py
+++ src/methods.py
@@ -75,7 +75,7 @@ def delete_reference(key):
     db.session.commit()
     return True
 
-def edit_reference(username, key, author, title, year, publisher, volume, series, address, edition, month, note):
+def edit_book(username, key, author, title, year, publisher, volume, series, address, edition, month, note):
     sql = text("UPDATE reference SET username = :username, author = :author, title = :title, year = :year, publisher = :publisher, volume = :volume, series = :series, address = :address, edition = :edition, month = :month, note = :note WHERE key = :key")
     db.session.execute(sql, {"username": username, "key": key, "author": author, "title": title, "year": year, "publisher": publisher, "volume": volume, "series": series, "address": address, "edition": edition, "month": month, "note":note})
     db.session.commit()
--- src/routes.py
+++ src/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, send_file
 import methods
 
 
